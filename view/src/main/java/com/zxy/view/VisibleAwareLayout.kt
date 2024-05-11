7562bbcf27efcc33ef0622aa0901cc3657c2be03
曾翔宇 <zengxiangyu@aggrx.com>
2024-05-11 11:22:11 +0800

    feat(view):更新可见性判断，支持viewpager，recylcerview，fragment的hide和show下view可见性的正确判断

--- view/src/main/java/com/zxy/view/VisibleAwareLayout.kt
+++ view/src/main/java/com/zxy/view/VisibleAwareLayout.kt
@@ -1,12 +1,17 @@
 package com.zxy.view
 
+import android.app.Activity
 import android.content.Context
+import android.content.res.Configuration
 import android.graphics.Rect
 import android.util.AttributeSet
-import android.util.DisplayMetrics
+import android.util.Log
+import android.view.View
 import android.view.ViewTreeObserver
-import android.view.WindowManager
 import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
 
 
 /**
@@ -17,24 +22,27 @@ open class VisibleAwareLayout : FrameLayout {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
+        context, attrs, defStyleAttr
     )
 
-    init {
-        val displayMetrics = DisplayMetrics()
-        (context.getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay.getRealMetrics(
-            displayMetrics
-        )
-        screenWidth = displayMetrics.widthPixels
-        screenHeight = displayMetrics.heightPixels
-    }
+    private var containerLeft = 0
+    private var containerRight = 0
+    private var containerTop = 0
+    private var containerBottom = 0
+    var visibleState: State = State.GONE
+        private set
+    private var windowIsVisible: Boolean = false
+    private var parentFragment: Fragment? = null;
+    var tagName = ""
 
-    private var screenWidth = 0
-    private var screenHeight = 0
-    private var lastVisibleStatus: Boolean = false
-    private var visiblePercent: Double = 1.0
+    //用于计算可见性判断的区域，默认为decorView
+    var basicRectView: View? = (context as? Activity)?.window?.decorView
+        set(value) {
+            field = value
+            refreshRectInfo()
+            handleListener()
+        }
+
     private val onGlobalLayoutListener = ViewTreeObserver.OnGlobalLayoutListener {
         handleListener()
     }
@@ -43,32 +51,112 @@ open class VisibleAwareLayout : FrameLayout {
     }
     var visibilityListener: VisibilityListener? = null
 
-    private fun isInScreen(): Boolean {
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        basicRectView?.post {
+            refreshRectInfo()
+            handleListener()
+        }
+    }
+
+    private fun refreshRectInfo() {
+        val rect = Rect()
+        basicRectView?.getGlobalVisibleRect(rect)
+        containerTop = rect.top
+        containerBottom = rect.bottom
+        containerLeft = rect.left
+        containerRight = rect.right
+    }
+
+    private fun getVisibleState(): State {
         if (!isAttachedToWindow) {
-            return false
+            return State.GONE
         }
         if (visibility != VISIBLE) {
-            return false
+            return State.GONE
         }
+        if (!windowIsVisible) {
+            return State.GONE
+        }
+        if (parentFragment?.isHidden == true) {
+            return State.GONE
+        }
+
         val globalRect = Rect()
         getGlobalVisibleRect(globalRect)
-        val rightOut = globalRect.right <= 0
-        val leftOut = globalRect.left >= screenWidth
-        val bottomOut =  globalRect.bottom <= 0
-        val topOut =  globalRect.top >= screenHeight
-        if (rightOut || leftOut || bottomOut || topOut){
-            return false
-        }
+        // 检查控件是否全部在容器的可视范围内
+        val globalRectAllVisible = (globalRect.top in containerTop until containerBottom)
+                && (globalRect.bottom in (containerTop + 1)..containerBottom)
+                && (globalRect.left in containerLeft until containerRight)
+                && (globalRect.right in (containerLeft + 1)..containerRight)
+        val globalRectPartiallyVisible =
+            (globalRect.left < containerRight || globalRect.right > containerLeft)
+                    && (globalRect.top < containerBottom && globalRect.bottom > containerTop)
+
         val drawingRect = Rect()
         getDrawingRect(drawingRect)
         val drawingArea =
             (drawingRect.bottom - drawingRect.top) * (drawingRect.right - drawingRect.left)
-        val localArea = (globalRect.bottom - globalRect.top) * (globalRect.right - globalRect.left)
-        return localArea.toDouble() / drawingArea >= visiblePercent
+        val globalArea = (globalRect.bottom - globalRect.top) * (globalRect.right - globalRect.left)
+        val areaShowAll = drawingArea == globalArea
+        if (BuildConfig.DEBUG) {
+            if (tagName.isNotEmpty()) {
+                Log.d(
+                    tagName,
+                    "container - $containerTop   $containerBottom   $containerLeft   $containerRight   "
+                )
+                Log.d(
+                    tagName,
+                    "globalRect - ${globalRect.top}   ${globalRect.bottom}   ${globalRect.left}   ${globalRect.right}   "
+                )
+                Log.d(
+                    tagName,
+                    "drawingRect - ${drawingRect.top}   ${drawingRect.bottom}   ${drawingRect.left}   ${drawingRect.right}   "
+                )
+                Log.d(tagName, "drawingArea $drawingArea    globalArea $globalArea")
+                Log.d(tagName, "globalRectAllVisible $globalRectAllVisible")
+                Log.d(tagName, "globalRectPartiallyVisible $globalRectPartiallyVisible")
+            }
+        }
+        val showAll = globalRectAllVisible && areaShowAll
+        val showRect = (!areaShowAll && globalRectAllVisible && globalRectPartiallyVisible)
+        if (BuildConfig.DEBUG) {
+            if (tagName.isNotEmpty()) {
+                Log.d(tagName, "showAll - $showAll")
+                Log.d(tagName, "showRect - $showRect")
+            }
+        }
+        return if (showAll) {
+            State.COMPLETELY_VISIBLE
+        } else if (showRect) {
+            State.PARTIALLY_VISIBLE
+        } else {
+            State.GONE
+        }
+    }
+
+    private fun getParentFragment(): Fragment? {
+        val fragmentManager = findFragmentManager(context)
+        if (fragmentManager != null) {
+            val fragments = fragmentManager.fragments
+            for (fragment in fragments) {
+                val view = fragment.view?.findViewById<View>(id)
+                if (view != null) {
+                    return fragment
+                }
+            }
+        }
+        return null
+    }
+
+    private fun findFragmentManager(context: Context): FragmentManager? {
+        return (context as? FragmentActivity)?.supportFragmentManager
     }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
+        parentFragment = getParentFragment()
+        refreshRectInfo()
         handleListener()
         viewTreeObserver.addOnGlobalLayoutListener(onGlobalLayoutListener)
         viewTreeObserver.addOnScrollChangedListener(onScrollChangedListener)
@@ -81,39 +169,50 @@ open class VisibleAwareLayout : FrameLayout {
         viewTreeObserver.removeOnScrollChangedListener(onScrollChangedListener)
     }
 
-    override fun onVisibilityAggregated(isVisible: Boolean) {
-        super.onVisibilityAggregated(isVisible)
-        handleListener()
-    }
-
     private fun handleListener() {
         post {
-            val currentStatus = isInScreen()
-            if (lastVisibleStatus == currentStatus) {
+            val currentState = getVisibleState()
+            if (visibleState == currentState) {
                 return@post
             }
-            lastVisibleStatus = currentStatus
-            if (currentStatus) {
-                visibilityListener?.onShow()
-            } else {
-                visibilityListener?.onDismiss()
-            }
+            visibleState = currentState
+            visibilityListener?.onVisibleStateChange(currentState)
         }
     }
 
-    fun setVisibleArea(percent: Double) {
-        visiblePercent = percent
+    override fun onWindowVisibilityChanged(visibility: Int) {
+        super.onWindowVisibilityChanged(visibility)
+        windowIsVisible = visibility == VISIBLE
+        handleListener()
     }
 
-    fun isVisible(): Boolean {
-        return lastVisibleStatus
+    fun visibleToUser(allExposure: Boolean = false): Boolean {
+        return if (allExposure) {
+            getVisibleState() == State.COMPLETELY_VISIBLE
+        } else {
+            getVisibleState() == State.COMPLETELY_VISIBLE || getVisibleState() == State.PARTIALLY_VISIBLE
+        }
+    }
+
+    enum class State {
+        /**
+         * 全部可见
+         */
+        COMPLETELY_VISIBLE,
+
+        /**
+         * 部分可见
+         */
+        PARTIALLY_VISIBLE,
+
+        /**
+         * 不可见
+         */
+        GONE
     }
 
     interface VisibilityListener {
-        /** 可见 */
-        fun onShow()
-
-        /** 不可见 */
-        fun onDismiss()
+        /** 可见性改变回调 */
+        fun onVisibleStateChange(state: State)
     }
 }
